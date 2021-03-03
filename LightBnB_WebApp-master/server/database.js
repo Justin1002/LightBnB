dc63ea3827837bc38d83fe0385df4295d57bb0de
Justin Ly <jly3@ualberta.ca>
2021-03-03 03:09:59 +0000

    add listing functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -117,7 +117,7 @@ const getAllProperties = function(options, limit = 10) {
 const queryParams = [];
 let query = `SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
-JOIN property_reviews ON properties.id = property_id
+LEFT JOIN property_reviews ON properties.id = property_id
 `
 if (options.city) {
   queryParams.push(`%${options.city}%`);
@@ -188,6 +188,12 @@ const addProperty = function(property) {
   // property.id = propertyId;
   // properties[propertyId] = property;
   // return Promise.resolve(property);
-  
+  const query = `INSERT into properties(owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,street,city,province,post_code,country,parking_spaces,number_of_bathrooms,number_of_bedrooms)
+  VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+  RETURNING *;`
+  const values = [property.owner_id,property.title,property.description,property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+  return pool.query(query, values)
+    .then(res => res.rows[0])
+    .catch(err => console.error('query error', err.stack));
 }
 exports.addProperty = addProperty;
